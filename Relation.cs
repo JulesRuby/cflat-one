bf877890a5a0e2475433588a1e2f782eacdaa08d
JulesRuby <JulesRuby@users.noreply.github.com>
2025-02-05 20:45:03 -0700

    Added in some functionality to Relation. Will come back to this after I test some Person func.

--- Relation.cs
+++ Relation.cs
@@ -2,20 +2,27 @@
 
 namespace creatingClasses
 {
+    internal class Relation
+    {
+        // constants
+        // fields
+        public string relationshipType;
 
-	internal class Relationship
-	{
-		public Relationship()
-		{
-			// constants
-			// fields
-			string relationshipType;
-			// properties
-			// constructor
-			// public
-			// private
-		}
+        // properties
+        public string RelationshipType { get; set; };
 
-	}
+        // constructor
+        public Relation(string relationshipType)
+        {
+            RelationshipType = relationshipType;
+        }
+
+        // public
+        public void showRelationshipType(Person person1, Person person2)
+        {
+            // Will code this part in once I get a better idea of how this is connected.
+        }
+        // private
+    }
 }
 
