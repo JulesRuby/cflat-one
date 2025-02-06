26f2445b3fe1d0c74d2db469ae81c6d0d08e5be7
JulesRuby <JulesRuby@users.noreply.github.com>
2025-02-05 21:06:14 -0700

    Added in the instantiation of for Person object just to test

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Tinder for lazy people.");
+
+            Person person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person person2 = new Person(2, "Gina", "James", "Green", 18, false);
+            Person person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            person1.displayPersonInfo();
+
+            Console.ReadLine();
         }
     }
 }
