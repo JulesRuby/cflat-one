1cabeae2dc53eb06430dbebd5ac733c813bb69c4
JulesRuby <JulesRuby@users.noreply.github.com>
2025-02-05 18:48:36 -0700

    Added Relation class file, just the bones.

---
+++ Relation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace creatingClasses
+{
+
+	internal class Relationship
+	{
+		public Relationship()
+		{
+			// constants
+			// fields
+			string relationshipType;
+			// properties
+			// constructor
+			// public
+			// private
+		}
+
+	}
+}
+
