53cfd40fe8e0f945d328c6b63c37f8a984754b3f
JulesRuby <JulesRuby@users.noreply.github.com>
2025-02-06 05:09:51 -0700

    Added in an enum to restrict allowed RelationshipType in the class. Created the output for the shoeRelationshipType() method.

--- Relation.cs
+++ Relation.cs
@@ -2,17 +2,26 @@
 
 namespace creatingClasses
 {
+    // Let's try to use an enum for valid RelationshipTypes
+    enum RelationshipTypes
+    {
+        Sister,
+        Brother,
+        Mother,
+        Father
+    }
+
     internal class Relation
     {
         // constants
         // fields
-        public string relationshipType;
+        //public string relationshipType;
 
         // properties
-        public string RelationshipType { get; set; }
+        public RelationshipTypes RelationshipType { get; set; }
 
         // constructor
-        public Relation(string relationshipType)
+        public Relation(RelationshipTypes relationshipType)
         {
             RelationshipType = relationshipType;
         }
@@ -21,6 +30,7 @@ namespace creatingClasses
         public void showRelationshipType(Person person1, Person person2)
         {
             // Will code this part in once I get a better idea of how this is connected.
+            Console.WriteLine($"Relationship between {person1.FirstName} and {person2.FirstName} is {RelationshipType}hood\n");
         }
         // private
     }
